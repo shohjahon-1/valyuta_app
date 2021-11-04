5a252c284a7d913cc89e4e1eca8bba4ce86b2ce1
◦•●◉ԲμҬμЯ૯ ૮ଇԁ૯Я◉●•◦ <81414536+7coder79@users.noreply.github.com>
2021-11-05 00:09:10 +0500

    installed http get inormations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:flag/flag.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,11 +11,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
       home:  MyHomePage(),
     );
   }
@@ -25,12 +28,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List json = [];
+  var url = Uri.parse('https://nbu.uz/uz/exchange-rates/json/');
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getResponse();
+  }
+
+  Future<void> getResponse() async {
+    var response = await http.get(url);
+
+    json = jsonDecode(response.body);
+    this.json = json;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Currency App'),
+      body: Center(
+        child: Column(
+          children: [
+            // TextButton(
+            //   child: Text('Get'),
+            //   onPressed: () {
+            //     // setState(() {
+            //     getResponse();
+            //     // });
+            //   },
+            // ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: json.length ?? 0,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      // DropdownButton(items: List<DropdownMenuItem<dynamic>> [
+                      //   json[index]['code'],
+                      // ]),
+                      ListTile(
+                        leading: Flag.fromString(
+                          json[index]['code'],
+                          height: 10,
+                          width: 100,
+                          fit: BoxFit.fill,
+                        ),
+                        title: Text(json[index]['title'].toString()),
+                        subtitle: Text(json[index]['cb_price'] + "so'm"),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
-    )
+    );
   }
 }
