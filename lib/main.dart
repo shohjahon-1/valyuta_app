f582e318e6101681154ccfe75456765ec99ce9e3
shohjahon <shohjahons936@gmail.com>
2021-11-05 08:46:34 +0500

    koring

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
